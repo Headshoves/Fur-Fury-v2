9da5e96d7f6bce3ce05462b9904f8db2647cd044
GuiLemes0607 <80102562+GuiLemes0607@users.noreply.github.com>
2022-11-07 11:33:22 -0300

    Som no menu e animas novas do bb

--- Fur-Fury/Assets/Scripts/Manager/menuManager.cs
+++ Fur-Fury/Assets/Scripts/Manager/menuManager.cs
@@ -6,17 +6,33 @@ using UnityEngine.SceneManagement;
 public class menuManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public AudioSource _audio;
+    public AudioClip _PressButton;
+    public float Time = 1f;
     void Start()
     {
        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return)) 
+
         {
-            SceneManager.LoadScene("SampleScene");
+
+            StartCoroutine(changeScene());
         }
     }
+    IEnumerator changeScene() 
+    {
+        _audio.clip = _PressButton;
+        _audio.Play();
+        yield return new WaitForSeconds(Time);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
 }
